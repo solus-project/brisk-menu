7819da3e05b3363d0f07474492c76b6dd21723c7
Ikey Doherty <ikey@solus-project.com>
2016-12-10 14:36:43 +0000

    Create skeleton for the SolMenuWindow type
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

---
+++ src/menu-window.c
@@ -0,0 +1,88 @@
+/*
+ * This file is part of mate-solmenu.
+ *
+ * Copyright © 2016 Ikey Doherty <ikey@solus-proejct.com>
+ *
+ * mate-solmenu is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ */
+
+#define _GNU_SOURCE
+
+#include "util.h"
+
+SOLUS_BEGIN_PEDANTIC
+#include "menu-window.h"
+#include <gtk/gtk.h>
+SOLUS_END_PEDANTIC
+
+struct _SolMenuWindowClass {
+        GtkWindowClass parent_class;
+};
+
+struct _SolMenuWindow {
+        GtkWindow parent;
+};
+
+G_DEFINE_TYPE(SolMenuWindow, sol_menu_window, GTK_TYPE_WINDOW)
+
+
+/**
+ * sol_menu_window_new:
+ *
+ * Construct a new SolMenuWindow object
+ */
+SolMenuWindow *sol_menu_window_new()
+{
+        return g_object_new(SOL_TYPE_MENU_WINDOW, NULL);
+}
+
+/**
+ * sol_menu_window_dispose:
+ *
+ * Clean up a SolMenuWindow instance
+ */
+static void sol_menu_window_dispose(GObject *obj)
+{
+        G_OBJECT_CLASS(sol_menu_window_parent_class)->dispose(obj);
+}
+
+/**
+ * sol_menu_window_class_init:
+ *
+ * Handle class initialisation
+ */
+static void sol_menu_window_class_init(SolMenuWindowClass *klazz)
+{
+        GObjectClass *obj_class = G_OBJECT_CLASS(klazz);
+
+        /* gobject vtable hookup */
+        obj_class->dispose = sol_menu_window_dispose;
+}
+
+/**
+ * sol_menu_window_init:
+ *
+ * Handle construction of the SolMenuWindow
+ */
+static void sol_menu_window_init(SolMenuWindow *self)
+{
+        /* TODO: Anything, really. */
+}
+
+
+/*
+ * Editor modelines  -  https://www.wireshark.org/tools/modelines.html
+ *
+ * Local variables:
+ * c-basic-offset: 8
+ * tab-width: 8
+ * indent-tabs-mode: nil
+ * End:
+ *
+ * vi: set shiftwidth=8 tabstop=8 expandtab:
+ * :indentSize=8:tabSize=8:noTabs=true:
+ */
+
---
+++ src/menu-window.h
@@ -0,0 +1,48 @@
+/*
+ * This file is part of mate-solmenu.
+ *
+ * Copyright © 2016 Ikey Doherty <ikey@solus-proejct.com>
+ *
+ * mate-solmenu is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ */
+
+#pragma once
+
+#include <glib-object.h>
+
+G_BEGIN_DECLS
+
+typedef struct _SolMenuWindow SolMenuWindow;
+typedef struct _SolMenuWindowClass SolMenuWindowClass;
+
+#define SOL_TYPE_MENU_WINDOW sol_menu_window_get_type()
+#define SOL_MENU_WINDOW(o)                                                                    \
+        (G_TYPE_CHECK_INSTANCE_CAST((o), SOL_TYPE_MENU_WINDOW, SolMenuWindow))
+#define SOL_IS_MENU_WINDOW(o) (G_TYPE_CHECK_INSTANCE_TYPE((o), SOL_TYPE_MENU_WINDOW))
+#define SOL_MENU_WINDOW_CLASS(o)                                                              \
+        (G_TYPE_CHECK_CLASS_CAST((o), SOL_TYPE_MENU_WINDOW, SolMenuWindowClass))
+#define SOL_IS_MENU_WINDOW_CLASS(o) (G_TYPE_CHECK_CLASS_TYPE((o), SOL_TYPE_MENU_WINDOW))
+#define SOL_MENU_WINDOW_GET_CLASS(o)                                                          \
+        (G_TYPE_INSTANCE_GET_CLASS((o), SOL_TYPE_MENU_WINDOW, SolMenuWindowClass))
+
+GtkWidget *sol_menu_window_new(void);
+
+GType sol_menu_window_get_type(void);
+
+G_END_DECLS
+
+/*
+ * Editor modelines  -  https://www.wireshark.org/tools/modelines.html
+ *
+ * Local variables:
+ * c-basic-offset: 8
+ * tab-width: 8
+ * indent-tabs-mode: nil
+ * End:
+ *
+ * vi: set shiftwidth=8 tabstop=8 expandtab:
+ * :indentSize=8:tabSize=8:noTabs=true:
+ */
