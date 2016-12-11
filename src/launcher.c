3a50594719e8aec22df2db0a97f20fd5334f6f5d
Ikey Doherty <ikey@solus-project.com>
2016-12-11 21:07:13 +0000

    launcher: Add initial tracking of app launches
    This allows us to end the startup notification for applications that
    provide it, to stop the spinny-wheel-of-frustration on the desktop.
    Currently we don't handle dbus startup apps
    
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- src/launcher.c
+++ src/launcher.c
@@ -29,9 +29,15 @@ struct _BriskMenuLauncherClass {
 struct _BriskMenuLauncher {
         GObject parent;
         GdkAppLaunchContext *context;
+        GdkDisplay *display;
 };
 
 G_DEFINE_TYPE(BriskMenuLauncher, brisk_menu_launcher, G_TYPE_OBJECT)
+
+static void brisk_menu_launcher_app_launched(BriskMenuLauncher *self, GAppInfo *info,
+                                             GVariant *data, GAppLaunchContext *context);
+static void brisk_menu_launcher_app_failed(BriskMenuLauncher *self, gchar *startup_id,
+                                           GAppLaunchContext *context);
 
 /**
  * brisk_menu_launcher_new:
@@ -82,6 +88,15 @@ static void brisk_menu_launcher_init(BriskMenuLauncher *self)
 
         display = gdk_display_get_default();
         self->context = gdk_display_get_app_launch_context(display);
+
+        g_signal_connect_swapped(self->context,
+                                 "launched",
+                                 G_CALLBACK(brisk_menu_launcher_app_launched),
+                                 self);
+        g_signal_connect_swapped(self->context,
+                                 "launch-failed",
+                                 G_CALLBACK(brisk_menu_launcher_app_launched),
+                                 self);
 }
 
 void brisk_menu_launcher_start(BriskMenuLauncher *self, GtkWidget *parent, GAppInfo *app_info)
@@ -102,9 +117,64 @@ void brisk_menu_launcher_start(BriskMenuLauncher *self, GtkWidget *parent, GAppI
                 gdk_app_launch_context_set_icon(self->context, icon);
         }
 
+        self->display = gdk_screen_get_display(screen);
+
         /* We may support DnD URIs onto the icons at some point, not for now. */
         g_app_info_launch(app_info, NULL, G_APP_LAUNCH_CONTEXT(self->context), NULL);
 }
+
+/**
+ * Handle the launch of an application so that we can remove the startup notification
+ * from the desktop environment.
+ */
+static void brisk_menu_launcher_app_launched(BriskMenuLauncher *self, GAppInfo *info,
+                                             GVariant *data, GAppLaunchContext *context)
+{
+        GVariantIter iter = { 0 };
+        GVariant *elem = NULL;
+        gchar *startup_id = NULL;
+        GdkDisplay *display = NULL;
+
+        g_variant_iter_init(&iter, data);
+        while ((elem = g_variant_iter_next_value(&iter)) != NULL) {
+                GVariant *value = NULL;
+                gchar *key = NULL;
+
+                g_variant_get(elem, "{sv}", &key, &value);
+
+                if (!g_variant_is_of_type(value, G_VARIANT_TYPE_STRING)) {
+                        continue;
+                }
+                if (g_str_equal(key, "startup-notification-id")) {
+                        g_variant_get(value, "s", &startup_id);
+                }
+                g_free(key);
+                g_variant_unref(value);
+
+                if (startup_id) {
+                        break;
+                }
+        }
+
+        if (!startup_id) {
+                return;
+        }
+
+        g_message("Launched: %s %s", g_app_info_get_name(info), startup_id);
+        gdk_display_notify_startup_complete(self->display, startup_id);
+        g_free(startup_id);
+}
+
+/**
+ * Handle the failure of an application to launch, so that we may notify the user
+ * about it, and also remove the startup notification from the desktop environment.
+ */
+static void brisk_menu_launcher_app_failed(BriskMenuLauncher *self, gchar *startup_id,
+                                           GAppLaunchContext *context)
+{
+        g_message("Failed to launch: %s", startup_id);
+}
+
 /*
  * Editor modelines  -  https://www.wireshark.org/tools/modelines.html
  *
