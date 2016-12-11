a8369b4031ff1a542fd504f250d73a60d8dfdd0c
Ikey Doherty <ikey@solus-project.com>
2016-12-11 13:21:13 +0000

    Reflow format
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- src/menu-search.c
+++ src/menu-search.c
@@ -1,8 +1,8 @@
 /*
  * This file is part of mate-solmenu.
- * 
+ *
  * Copyright © 2016 Ikey Doherty <ikey@solus-project.com>
- * 
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
