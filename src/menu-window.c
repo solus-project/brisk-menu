3ed77a915e37425fa15443dee33f209fa0363496
Ikey Doherty <ikey@solus-project.com>
2016-12-11 13:20:07 +0000

    Fix drastically broken licence headers
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- src/menu-window.c
+++ src/menu-window.c
@@ -1,12 +1,12 @@
 /*
  * This file is part of mate-solmenu.
- *
- * Copyright © 2016 Ikey Doherty <ikey@solus-proejct.com>
- *
- * mate-solmenu is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation; either version 2.1
- * of the License, or (at your option) any later version.
+ * 
+ * Copyright © 2016 Ikey Doherty <ikey@solus-project.com>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  */
 
 #define _GNU_SOURCE
