b064f5ace99af9b868e7f40df2ead05b8eb683da
Ikey Doherty <ikey@solus-project.com>
2016-12-21 12:04:18 +0000

    Add non-functional skeleton for the applet itself
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

---
+++ src/applet.c
@@ -0,0 +1,75 @@
+/*
+ * This file is part of brisk-menu.
+ *
+ * Copyright © 2016 Ikey Doherty <ikey@solus-project.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ */
+
+#define _GNU_SOURCE
+
+#include "util.h"
+
+BRISK_BEGIN_PEDANTIC
+#include "applet.h"
+#include <gtk/gtk.h>
+#include <mate-panel-applet.h>
+BRISK_END_PEDANTIC
+
+struct _BriskMenuAppletClass {
+        MatePanelAppletClass parent_class;
+};
+
+struct _BriskMenuApplet {
+        MatePanelApplet parent;
+};
+
+G_DEFINE_TYPE(BriskMenuApplet, brisk_menu_applet, PANEL_TYPE_APPLET)
+
+/**
+ * brisk_menu_applet_dispose:
+ *
+ * Clean up a BriskMenuApplet instance
+ */
+static void brisk_menu_applet_dispose(GObject *obj)
+{
+        G_OBJECT_CLASS(brisk_menu_applet_parent_class)->dispose(obj);
+}
+
+/**
+ * brisk_menu_applet_class_init:
+ *
+ * Handle class initialisation
+ */
+static void brisk_menu_applet_class_init(BriskMenuAppletClass *klazz)
+{
+        GObjectClass *obj_class = G_OBJECT_CLASS(klazz);
+
+        /* gobject vtable hookup */
+        obj_class->dispose = brisk_menu_applet_dispose;
+}
+
+/**
+ * brisk_menu_applet_init:
+ *
+ * Handle construction of the BriskMenuApplet
+ */
+static void brisk_menu_applet_init(BriskMenuApplet *self)
+{
+}
+
+/*
+ * Editor modelines  -  https://www.wireshark.org/tools/modelines.html
+ *
+ * Local variables:
+ * c-basic-offset: 8
+ * tab-width: 8
+ * indent-tabs-mode: nil
+ * End:
+ *
+ * vi: set shiftwidth=8 tabstop=8 expandtab:
+ * :indentSize=8:tabSize=8:noTabs=true:
+ */
---
+++ src/applet.h
@@ -0,0 +1,47 @@
+/*
+ * This file is part of brisk-menu.
+ *
+ * Copyright © 2016 Ikey Doherty <ikey@solus-project.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ */
+
+#pragma once
+
+#include <glib-object.h>
+#include <gtk/gtk.h>
+
+G_BEGIN_DECLS
+
+typedef struct _BriskMenuApplet BriskMenuApplet;
+typedef struct _BriskMenuAppletClass BriskMenuAppletClass;
+
+#define BRISK_TYPE_MENU_APPLET brisk_menu_applet_get_type()
+#define BRISK_MENU_APPLET(o)                                                                       \
+        (G_TYPE_CHECK_INSTANCE_CAST((o), BRISK_TYPE_MENU_APPLET, BriskMenuApplet))
+#define BRISK_IS_MENU_APPLET(o) (G_TYPE_CHECK_INSTANCE_TYPE((o), BRISK_TYPE_MENU_APPLET))
+#define BRISK_MENU_APPLET_CLASS(o)                                                                 \
+        (G_TYPE_CHECK_CLASS_CAST((o), BRISK_TYPE_MENU_APPLET, BriskMenuAppletClass))
+#define BRISK_IS_MENU_APPLET_CLASS(o) (G_TYPE_CHECK_CLASS_TYPE((o), BRISK_TYPE_MENU_APPLET))
+#define BRISK_MENU_APPLET_GET_CLASS(o)                                                             \
+        (G_TYPE_INSTANCE_GET_CLASS((o), BRISK_TYPE_MENU_APPLET, BriskMenuAppletClass))
+
+GType brisk_menu_applet_get_type(void);
+
+G_END_DECLS
+
+/*
+ * Editor modelines  -  https://www.wireshark.org/tools/modelines.html
+ *
+ * Local variables:
+ * c-basic-offset: 8
+ * tab-width: 8
+ * indent-tabs-mode: nil
+ * End:
+ *
+ * vi: set shiftwidth=8 tabstop=8 expandtab:
+ * :indentSize=8:tabSize=8:noTabs=true:
+ */
