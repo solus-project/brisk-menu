cd009fb9d6a6dd187bc21f57cc09541c9d98ca6b
Ikey Doherty <ikey@solus-project.com>
2017-01-19 22:36:43 +0000

    sort: Only sort using GDesktopAppInfo
    This will help us to remove use of the menu tree entry for consistency.
    
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- src/menu-sort.c
+++ src/menu-sort.c
@@ -23,13 +23,13 @@ BRISK_END_PEDANTIC
 /**
  * Compute a score for the given entry based on the input term.
  */
-static gint brisk_get_entry_score(MateMenuTreeEntry *entry, gchar *term)
+static gint brisk_get_entry_score(GAppInfo *info, gchar *term)
 {
         gint score = 0;
         autofree(gchar) *name = NULL;
         char *find = NULL;
 
-        name = g_ascii_strdown(matemenu_tree_entry_get_name(entry), -1);
+        name = g_ascii_strdown(g_app_info_get_name(info), -1);
         if (g_str_equal(name, term)) {
                 score += 100;
         } else if (g_str_has_prefix(name, term)) {
@@ -49,7 +49,7 @@ static gint brisk_get_entry_score(MateMenuTreeEntry *entry, gchar *term)
 gint brisk_menu_window_sort(GtkListBoxRow *row1, GtkListBoxRow *row2, gpointer v)
 {
         GtkWidget *child1, *child2 = NULL;
-        MateMenuTreeEntry *entryA, *entryB = NULL;
+        GAppInfo *infoA, *infoB = NULL;
         autofree(gchar) *nameA = NULL;
         autofree(gchar) *nameB = NULL;
         BriskMenuWindow *self = NULL;
@@ -59,18 +59,18 @@ gint brisk_menu_window_sort(GtkListBoxRow *row1, GtkListBoxRow *row2, gpointer v
         child1 = gtk_bin_get_child(GTK_BIN(row1));
         child2 = gtk_bin_get_child(GTK_BIN(row2));
 
-        g_object_get(child1, "entry", &entryA, NULL);
-        g_object_get(child2, "entry", &entryB, NULL);
+        g_object_get(child1, "info", &infoA, NULL);
+        g_object_get(child2, "info", &infoB, NULL);
 
         if (self->search_term) {
-                gint sc1 = brisk_get_entry_score(entryA, self->search_term);
-                gint sc2 = brisk_get_entry_score(entryB, self->search_term);
+                gint sc1 = brisk_get_entry_score(infoA, self->search_term);
+                gint sc2 = brisk_get_entry_score(infoB, self->search_term);
                 return (sc1 > sc2) - (sc1 - sc2);
         }
 
         /* Ensure we compare lower case only */
-        nameA = g_ascii_strdown(matemenu_tree_entry_get_display_name(entryA), -1);
-        nameB = g_ascii_strdown(matemenu_tree_entry_get_display_name(entryB), -1);
+        nameA = g_ascii_strdown(g_app_info_get_display_name(infoA), -1);
+        nameB = g_ascii_strdown(g_app_info_get_display_name(infoB), -1);
         return g_strcmp0(nameA, nameB);
 }
 
