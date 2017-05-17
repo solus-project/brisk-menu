2dd16a6e13db914094a63d1d467f1c527d3c2f0e
Ikey Doherty <ikey@solus-project.com>
2017-05-17 01:09:11 +0100

    backends: Add skeletal definition for a BriskItem
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

---
+++ src/backend/item.c
@@ -0,0 +1,133 @@
+/*
+ * This file is part of brisk-menu.
+ *
+ * Copyright © 2017 Ikey Doherty <ikey@solus-project.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ */
+
+#define _GNU_SOURCE
+
+#include "util.h"
+
+BRISK_BEGIN_PEDANTIC
+#include "item.h"
+BRISK_END_PEDANTIC
+
+G_DEFINE_TYPE(BriskItem, brisk_item, G_TYPE_OBJECT)
+
+/**
+ * brisk_item_dispose:
+ *
+ * Clean up a BriskItem instance
+ */
+static void brisk_item_dispose(GObject *obj)
+{
+        G_OBJECT_CLASS(brisk_item_parent_class)->dispose(obj);
+}
+
+/**
+ * brisk_item_class_init:
+ *
+ * Handle class initialisation
+ */
+static void brisk_item_class_init(BriskItemClass *klazz)
+{
+        GObjectClass *obj_class = G_OBJECT_CLASS(klazz);
+
+        /* gobject vtable hookup */
+        obj_class->dispose = brisk_item_dispose;
+}
+
+/**
+ * brisk_item_init:
+ *
+ * Handle construction of the BriskItem
+ */
+static void brisk_item_init(__brisk_unused__ BriskItem *self)
+{
+}
+
+/**
+ * brisk_item_get_id:
+ *
+ * Returns the unique ID for this item within the backend
+ * @note This string belongs to the backend, and must not be freed by the caller
+ */
+const gchar *brisk_item_get_id(BriskItem *item)
+{
+        g_assert(item != NULL);
+        BriskItemClass *klazz = BRISK_ITEM_GET_CLASS(item);
+        g_assert(klazz->get_id != NULL);
+        return klazz->get_id(item);
+}
+
+/**
+ * brisk_item_get_name:
+ *
+ * Returns the item name used when display the item in the menu
+ * @note This string belongs to the backend, and must not be freed by the caller
+ */
+const gchar *brisk_item_get_name(BriskItem *item)
+{
+        g_assert(item != NULL);
+        BriskItemClass *klazz = BRISK_ITEM_GET_CLASS(item);
+        g_assert(klazz->get_name != NULL);
+        return klazz->get_name(item);
+}
+
+/**
+ * brisk_item_get_summary:
+ *
+ * Returns the summary used when display the item in the menu
+ * @note This string belongs to the backend, and must not be freed by the caller
+ */
+const gchar *brisk_item_get_summary(BriskItem *item)
+{
+        g_assert(item != NULL);
+        BriskItemClass *klazz = BRISK_ITEM_GET_CLASS(item);
+        g_assert(klazz->get_summary != NULL);
+        return klazz->get_summary(item);
+}
+
+/**
+ * brisk_item_matches_search:
+ *
+ * Returns true if the item matches the given search term
+ */
+gboolean brisk_item_matches_search(BriskItem *item, gchar *term)
+{
+        g_assert(item != NULL);
+        BriskItemClass *klazz = BRISK_ITEM_GET_CLASS(item);
+        g_return_val_if_fail(klazz->matches_search != NULL, FALSE);
+        return klazz->matches_search(item, term);
+}
+
+/**
+ * brisk_item_launch:
+ *
+ * Attempt to launch the item
+ */
+gboolean brisk_item_launch(BriskItem *item)
+{
+        g_assert(item != NULL);
+        BriskItemClass *klazz = BRISK_ITEM_GET_CLASS(item);
+        g_return_val_if_fail(klazz->launch != NULL, FALSE);
+        return klazz->launch(item);
+}
+
+/*
+ * Editor modelines  -  https://www.wireshark.org/tools/modelines.html
+ *
+ * Local variables:
+ * c-basic-offset: 8
+ * tab-width: 8
+ * indent-tabs-mode: nil
+ * End:
+ *
+ * vi: set shiftwidth=8 tabstop=8 expandtab:
+ * :indentSize=8:tabSize=8:noTabs=true:
+ */
---
+++ src/backend/item.h
@@ -0,0 +1,79 @@
+/*
+ * This file is part of brisk-menu.
+ *
+ * Copyright © 2017 Ikey Doherty <ikey@solus-project.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ */
+
+#pragma once
+
+#include <glib-object.h>
+
+G_BEGIN_DECLS
+
+typedef struct _BriskItem BriskItem;
+typedef struct _BriskItemClass BriskItemClass;
+
+struct _BriskItemClass {
+        GObjectClass parent_class;
+
+        /* These must be implemented by subclasses */
+        const gchar *(*get_id)(BriskItem *);
+        const gchar *(*get_name)(BriskItem *);
+        const gchar *(*get_summary)(BriskItem *);
+
+        /* If the subclass supports searching, override this */
+        gboolean (*matches_search)(BriskItem *, gchar *);
+
+        /* Support launching through primary click action */
+        gboolean (*launch)(BriskItem *);
+
+        gpointer padding[12];
+};
+
+/**
+ * BriskItem is an abstract top-level class which is used as the base
+ * of all other backend implementations within Brisk.
+ */
+struct _BriskItem {
+        GObject parent;
+};
+
+#define BRISK_TYPE_ITEM brisk_item_get_type()
+#define BRISK_ITEM(o) (G_TYPE_CHECK_INSTANCE_CAST((o), BRISK_TYPE_ITEM, BriskItem))
+#define BRISK_IS_ITEM(o) (G_TYPE_CHECK_INSTANCE_TYPE((o), BRISK_TYPE_ITEM))
+#define BRISK_ITEM_CLASS(o) (G_TYPE_CHECK_CLASS_CAST((o), BRISK_TYPE_ITEM, BriskItemClass))
+#define BRISK_IS_ITEM_CLASS(o) (G_TYPE_CHECK_CLASS_TYPE((o), BRISK_TYPE_ITEM))
+#define BRISK_ITEM_GET_CLASS(o) (G_TYPE_INSTANCE_GET_CLASS((o), BRISK_TYPE_ITEM, BriskItemClass))
+
+GType brisk_item_get_type(void);
+
+/* API Methods Follow */
+
+/* Core vfuncs required by everyone */
+const gchar *brisk_item_get_id(BriskItem *item);
+const gchar *brisk_item_get_name(BriskItem *item);
+const gchar *brisk_item_get_summary(BriskItem *item);
+gboolean brisk_item_matches_search(BriskItem *item, gchar *term);
+
+/* Attempt to load for the first time */
+gboolean brisk_item_launch(BriskItem *backend);
+
+G_END_DECLS
+
+/*
+ * Editor modelines  -  https://www.wireshark.org/tools/modelines.html
+ *
+ * Local variables:
+ * c-basic-offset: 8
+ * tab-width: 8
+ * indent-tabs-mode: nil
+ * End:
+ *
+ * vi: set shiftwidth=8 tabstop=8 expandtab:
+ * :indentSize=8:tabSize=8:noTabs=true:
+ */
